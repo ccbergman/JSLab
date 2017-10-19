abf0361f77f9c1d253a8e2897ac445b50b2fb0fd
Chelsea Bergman <chelseacbergman@gmail.com>
2017-10-19 16:03:30 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var game = prompt("Do you want to play?");
+var character1 = "user";
+var character2 = "Almighty Grant";
+
+if (game === "yes") {
+    var userName = prompt("What's your name?");
+} else {
+    console.log("...maybe next time.")
+}
