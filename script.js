b63e054aea0d9db28b3667cc556d584e6bae0129
Chelsea Bergman <chelseacbergman@gmail.com>
2017-10-19 16:50:40 -0400

    prompts

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 var game = prompt("Do you want to play?");
-var character1 = "user";
-var character2 = "Almighty Grant";
+var char1 = "user";
+var char2 = "Almighty Grant";
+var usrWins = 0;
+var grantWins = 0;
+
+//don't get this part..
+// var random = function math.random(math.floor() * 2) + 1;
+
 
 if (game === "yes") {
-    var userName = prompt("What's your name?");
+    var char1 = prompt("What's your name?");
 } else {
     console.log("...maybe next time.")
 }
+
+//don't know how to start this part
+// while (usrWins > 0 && grantWins > 0) {
+//     console.log("char1Points");
+// }
+
+// test if username was overwritten
+// console.log(char1);
