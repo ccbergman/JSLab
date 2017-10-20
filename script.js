8239fb8861fa183d5087edae558a41461b9aa84d
Chelsea Bergman <chelseacbergman@gmail.com>
2017-10-19 20:38:48 -0400

    add while loop pt1

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-var game = prompt("Do you want to play?");
+// var game = prompt("Do you want to play?");
 var char1 = "user";
 var char2 = "Almighty Grant";
 var usrWins = 0;
-var grantWins = 0;
+var usrBaseHp = 40;
+var grantBaseHp = 10;
+var usrCurrentHp = 0;
+var grantCurrentHp = 0;
 
-//don't get this part..
+// var random = math.floor(math.random() * 2) + 1;
+// var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
 // var random = function math.random(math.floor() * 2) + 1;
 
 
-if (game === "yes") {
-    var char1 = prompt("What's your name?");
-} else {
-    console.log("...maybe next time.")
-}
-
-//don't know how to start this part
-// while (usrWins > 0 && grantWins > 0) {
-//     console.log("char1Points");
+// if (game === "yes") {
+//     char1 = prompt("What's your name?");
+// } else {
+//     console.log("ok...maybe next time.")
 // }
 
+while (grantBaseHp >= 0) {
+    grantBaseHp--;
+    console.log("Grant the Almighty Chicken has " + grantBaseHp + " health left.");
+}
+
+usrWins++;
+console.log(char1 + " Wins: " + usrWins);
+
 // test if username was overwritten
-// console.log(char1);
+// console.log(char1)
+//still ply game when say no
