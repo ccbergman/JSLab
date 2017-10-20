43f01acb90269c3a46bb10943306daa7376c7a88
Chelsea Bergman <chelseacbergman@gmail.com>
2017-10-19 20:56:51 -0400

    rounds working

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// var game = prompt("Do you want to play?");
+var game = prompt("Do you want to play?");
 var char1 = "user";
 var char2 = "Almighty Grant";
 var usrWins = 0;
 var usrBaseHp = 40;
 var grantBaseHp = 10;
 var usrCurrentHp = 0;
-var grantCurrentHp = 0;
+var grantCurrentHp = grantBaseHp;
 
 // var random = math.floor(math.random() * 2) + 1;
 // var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
@@ -13,20 +13,25 @@ var grantCurrentHp = 0;
 // var random = function math.random(math.floor() * 2) + 1;
 
 
-// if (game === "yes") {
-//     char1 = prompt("What's your name?");
-// } else {
-//     console.log("ok...maybe next time.")
-// }
-
-while (grantBaseHp >= 0) {
-    grantBaseHp--;
-    console.log("Grant the Almighty Chicken has " + grantBaseHp + " health left.");
+if (game === "yes") {
+    char1 = prompt("What's your name?");
+    singleRound();
+    singleRound();
+    singleRound();
+} else {
+    console.log("Ok...maybe next time.")
 }
 
-usrWins++;
-console.log(char1 + " Wins: " + usrWins);
+function singleRound() {
+    while (0 < grantCurrentHp) {
+        grantCurrentHp--;
+        console.log("Grant the Almighty Chicken has " + grantCurrentHp + " health left.");
+    }
+    usrWins++;
+    console.log(char1 + " Wins: " + usrWins);
+    grantCurrentHp = grantBaseHp;
+}
 
 // test if username was overwritten
 // console.log(char1)
-//still ply game when say no
+//remove unnecessary console prints
